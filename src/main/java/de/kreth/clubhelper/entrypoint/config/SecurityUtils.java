b991f47b35c073f58219510782faf7990b31e43c
Markus Kreth <markus.kreth@web.de>
2022-03-28 22:26:17 +0200

    Corrected Keycloak security configuration

--- src/main/java/de/kreth/clubhelper/entrypoint/config/SecurityUtils.java
+++ src/main/java/de/kreth/clubhelper/entrypoint/config/SecurityUtils.java
@@ -11,26 +11,25 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import com.vaadin.flow.shared.ApplicationConstants;
 
 public class SecurityUtils {
-    /**
-     * Tests if the request is an internal framework request. The test consists of
-     * checking if the request parameter is present and if its value is consistent
-     * with any of the request types know.
-     *
-     * @param request {@link HttpServletRequest}
-     * @return true if is an internal framework request. False otherwise.
-     */
-    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
-	final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
+	/**
+	 * Tests if the request is an internal framework request. The test consists of
+	 * checking if the request parameter is present and if its value is consistent
+	 * with any of the request types know.
+	 *
+	 * @param request {@link HttpServletRequest}
+	 * @return true if is an internal framework request. False otherwise.
+	 */
+	static boolean isFrameworkInternalRequest(HttpServletRequest request) {
+		final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
 
-	return parameterValue != null
-		&& Stream.of(com.vaadin.flow.server.HandlerHelper.RequestType.values())
-			.anyMatch(r -> r.getIdentifier().equals(parameterValue));
-    }
+		return parameterValue != null && Stream.of(com.vaadin.flow.server.HandlerHelper.RequestType.values())
+				.anyMatch(r -> r.getIdentifier().equals(parameterValue));
+	}
 
-    static boolean isUserLoggedIn() {
-	Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); //
-	return authentication != null //
-		&& !(authentication instanceof AnonymousAuthenticationToken) //
-		&& authentication.isAuthenticated(); //
-    }
+	static boolean isUserLoggedIn() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); //
+		return authentication != null //
+				&& !(authentication instanceof AnonymousAuthenticationToken) //
+				&& authentication.isAuthenticated(); //
+	}
 }
--- src/main/java/de/kreth/clubhelper/entrypoint/config/UiSecurityConfig.java
+++ src/main/java/de/kreth/clubhelper/entrypoint/config/UiSecurityConfig.java
@@ -4,13 +4,9 @@ import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
-import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -22,69 +18,58 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @KeycloakConfiguration
 public class UiSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    @Autowired
-    private KeycloakClientRequestFactory factory;
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) {
+		KeycloakAuthenticationProvider keyCloakAuthProvider = keycloakAuthenticationProvider();
+		keyCloakAuthProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+		auth.authenticationProvider(keyCloakAuthProvider);
+	}
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) {
-	KeycloakAuthenticationProvider keyCloakAuthProvider = keycloakAuthenticationProvider();
-	keyCloakAuthProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-	auth.authenticationProvider(keyCloakAuthProvider);
-    }
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
 
-    @Bean
-    public KeycloakConfigResolver keyCloakConfigResolver() {
-	return new KeycloakSpringBootConfigResolver();
-    }
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http
+			.csrf().disable()
+			.anonymous().disable()
+			.authorizeRequests().requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
+			.anyRequest().hasAnyRole("ROLE_trainer", "ROLE_admin");
 
-    @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public KeycloakRestTemplate restTemplate() {
-	return new KeycloakRestTemplate(factory);
-    }
+	}
 
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-	return new RegisterSessionAuthenticationStrategy(
-		new SessionRegistryImpl());
-    }
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(
+				// Vaadin Flow static resources //
+				"/VAADIN/**",
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-	super.configure(http);
-	http.csrf().disable()
-		.authorizeRequests()
-		.requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
-		.antMatchers("admin").fullyAuthenticated()
-		.anyRequest().permitAll();
-    }
+				// the standard favicon URI
+				"/favicon.ico",
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-	web.ignoring().antMatchers(
-		// Vaadin Flow static resources //
-		"/VAADIN/**",
+				// the robots exclusion standard
+				"/robots.txt",
 
-		// the standard favicon URI
-		"/favicon.ico",
+				// web application manifest //
+				"/manifest.webmanifest", "/sw.js", "/offline-page.html",
 
-		// the robots exclusion standard
-		"/robots.txt",
+				// (development mode) static resources //
+				"/frontend/**",
 
-		// web application manifest //
-		"/manifest.webmanifest",
-		"/sw.js",
-		"/offline-page.html",
+				// (development mode) webjars //
+				"/webjars/**",
 
-		// (development mode) static resources //
-		"/frontend/**",
+				// (production mode) static resources //
+				"/frontend-es5/**", "/frontend-es6/**");
+	}
 
-		// (development mode) webjars //
-		"/webjars/**",
-
-		// (production mode) static resources //
-		"/frontend-es5/**", "/frontend-es6/**");
-    }
+	@Bean
+	public static KeycloakConfigResolver keycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
 
 }
